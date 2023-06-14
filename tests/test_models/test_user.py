ac77cf10f860eca576a89308bdaa7649ef6f7909
jefftrojan <j.dauda@alustudent.com>
2023-06-14 18:37:33 +0200

    Added test files for console

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,12 +16,12 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(type(new.first_name), None)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(type(new.last_name), None)
 
     """def test_email(self):
         """ """
@@ -32,4 +32,4 @@ class test_User(test_basemodel):
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(new.password), None)
